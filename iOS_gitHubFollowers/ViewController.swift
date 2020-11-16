a47914411e25b420a20c6e728fd3390aa9e19e28
Abdelrady <abdelrady@Abdelradys-Mac.local>
2020-11-16 19:57:45 +0200

    starting without story board

---
+++ iOS_gitHubFollowers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS_gitHubFollowers
+//
+//  Created by Abdelrady on 11/15/20.
+//  Copyright © 2020 Abdelrady. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .brown
+    }
+
+
+}
+
